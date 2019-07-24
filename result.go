0a07fbf35d9f0dc20e4cc76414b72a5949ac611e
Charles Iliya Krempeaux <iamreiver@gmail.com>
2019-07-23 17:53:19 -0700

    denary.Result.Unwrap()

--- result.go
+++ result.go
@@ -14,3 +14,13 @@ func NoResult() Result {
 	return Result{}
 }
 
+func (receiver Result) Unwrap() (Type, bool) {
+	if NoResult() == receiver {
+		return Type{}, false
+	}
+	if receiver.errored() {
+		return Type{}, false
+	}
+
+	return Type{receiver.value}, true
+}
